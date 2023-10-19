07de4605c8312ae37ab1a452b71a259418bae398
Peester <oliversladkovsky@gmail.com>
2023-10-19 23:02:06 +0200

    React 2 - rest operator

--- src/SpreadRestOperator.jsx
+++ src/SpreadRestOperator.jsx
@@ -2,29 +2,21 @@ import React from 'react'
 
 const SpreadRestOperator = () => {
 
-    const myArray = ["Harry", "Ron"]
-    const mySecArray = [...myArray, "Hermiona"]
 
-    const myArray2 = [1,2,3]
+  const sum = (...numbers) => {
+    let result = 0
 
-    const myFunc1 = (a, b, c) => {
-        return a + b + c 
+    for(const i of numbers ){
+      result = result + i // result += i
     }
+    return result
+  }
 
-    console.log(myFunc1(...myArray2))
-
-    const myArray3 = ["Ron", "Hermiona", "Harry"]
-    const myArray4 = [...myArray3, "Dobby"]
-
-    console.log(myArray4)
-    console.log(myArray3)
 
   return (
     <div className='spreadAndRestOperator'>
     <div className='spread'>
-        {mySecArray.map((name,index) => {
-            return <p key={index}>{name}</p>
-        })}
+    <p>{sum(1,5,2,2,20)}</p>
     </div>
 </div>
   )
